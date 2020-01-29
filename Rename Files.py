32defb7fa0152ff210dd7cf96bc0c601f9b83168
Ole Reichhelm <ole.reichhelm@t-online.de>
2020-01-29 15:12:52 +0100

    Custom path via cli

--- Rename Files.py
+++ Rename Files.py
@@ -1,8 +1,9 @@
 import os
-path = 'D:/+Upload-(danach-loeschen)/2009'
+path = raw_input("Bitte gib den Pfad an, in dem die Dateien liegen.")
 files = os.listdir(path)
-newName = raw_input("Bitte gib die Jahreszahl ein :) ")
+
+newName = raw_input("Was soll vor die Dateien geschrieben werden?")
 
 
 for index, file in enumerate(files):
-     os.rename(os.path.join(path, file), os.path.join(path, newName+'-'+file))
+    os.rename(os.path.join(path, file), os.path.join(path, newName+'-'+file))
